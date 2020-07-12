6f9b9d7f6573346ab69399926d619b847e8f3f17
Tom Drapes <tjdrapes@gmail.com>
2020-07-12 14:28:22 +1000

    removed dist folder

--- api/functions/dist/register.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = void 0;
-const utils_1 = require("utils");
-exports.handler = async (event) => {
-    return utils_1.addDocument("User", JSON.parse(event.body));
-};
